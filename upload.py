b7edc6ecdefc6edd0f58d7d45ac7f3e14d772409
basatsu <97040668+basatsu@users.noreply.github.com>
2023-02-09 00:16:19 +0900

    add progress

--- upload.py
+++ upload.py
@@ -1,54 +1,79 @@
-import flet as ft
-from simpledatatable import DataFrame
+from typing import Dict
+
+import flet
+from flet import (
+    Column,
+    ElevatedButton,
+    FilePicker,
+    FilePickerResultEvent,
+    FilePickerUploadEvent,
+    FilePickerUploadFile,
+    Page,
+    ProgressRing,
+    Ref,
+    Row,
+    Text,
+    icons,
+    Divider
+)
 import pandas as pd
 
 
-def main(page: ft.Page):
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        selected_files.value = (
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
-        )
-        selected_files.update()
+def main(page: Page):
+    prog_bars: Dict[str, ProgressRing] = {}
+    files = Ref[Column]()
+    upload_button = Ref[ElevatedButton]()
 
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.Text()
+    def file_picker_result(e: FilePickerResultEvent):
+        upload_button.current.disabled = True if e.files is None else False
+        prog_bars.clear()
+        files.current.controls.clear()
+        if e.files is not None:
+            for f in e.files:
+                prog = ProgressRing(value=0, bgcolor="#eeeeee", width=20, height=20)
+                prog_bars[f.name] = prog
+                files.current.controls.append(Row([prog, Text(f.name)]))
+        page.update()
 
-    page.overlay.append(pick_files_dialog)
+    def on_upload_progress(e: FilePickerUploadEvent):
+        prog_bars[e.file_name].value = e.progress
+        prog_bars[e.file_name].update()
+
+    file_picker = FilePicker(on_result=file_picker_result, on_upload=on_upload_progress)
+
+    def upload_files(e):
+        global rowcount
+        global colcount
+        if file_picker.result is not None and file_picker.result.files is not None:
+            for f in file_picker.result.files:
+                df=pd.read_csv(f.name)
+                rowcount=len(df)
+                colcount=len(df.columns)
+
+
+    # hide dialog in a overlay
+    page.overlay.append(file_picker)
+
 
     page.add(
-        ft.Row(
-            [
-                ft.ElevatedButton(
-                    "Pick files",
-                    icon=ft.icons.UPLOAD_FILE,
-                    on_click=lambda _: pick_files_dialog.pick_files(
-                        allow_multiple=True
-                    ),
-                ),
-                selected_files,
-            ]
-        )
+        ElevatedButton(
+            "Select files...",
+            icon=icons.FOLDER_OPEN,
+            on_click=lambda _: file_picker.pick_files(allow_multiple=True),
+        ),
+        Column(ref=files),
+        ElevatedButton(
+            "Upload",
+            ref=upload_button,
+            icon=icons.UPLOAD,
+            on_click=upload_files,
+            disabled=True,
+        ),
+        Text(rowcount),
+        Divider(height=9, thickness=3),
+        Text(colcount)
+
     )
-    
-    if selected_files != None:
-        df = pd.read_csv(selected_files)
-        simpledt_df = DataFrame(df)  # Initialize simpledt DataFrame object
-        simpledt_dt = simpledt_df.datatable  # Extract DataTable instance from simpledt
-        
-        simpledt_dt.bgcolor = ft.colors.RED # Change background color of generated DataTable
-        simpledt_dt.border = ft.border.all(10, ft.colors.PINK_600) # Add ping border to DataTable
-        dr = simpledt_df.datarows
 
-        for i in dr:
-            rownum = i.cells[0].content.value
-            if int(rownum) % 1 == 0:
-                i.color = ft.colors.GREEN
-        
-        dc = simpledt_df.datacolumns
 
-        for i in dc:
-            i.label=ft.Row([i.label, ft.Icon(ft.icons.AC_UNIT)])
-        
-        page.add(simpledt_dt)
-
-ft.app(target=main)
+flet.app(target=main)
